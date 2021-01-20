9fefde4eff2b556b037ab2e4dc84be1bc6696b5f
Romain Vacheret <romain.vacheret@gmail.com>
2021-01-20 13:05:33 +0100

    feat: add summary visitor

--- pyparser/summary/summary.py
+++ pyparser/summary/summary.py
@@ -1,4 +1,21 @@
+import inspect
+
 from json import dumps
+
+# Predicates
+# All attributes
+all_attrs_predicate = lambda : True
+# Attributes which are not in `attrs`
+excluded_attrs_predicate = lambda key, excluded_attrs: key not in excluded_attrs
+# Attributes which are not None
+attr_not_none_predicate = lambda value: value is not None
+# Attributes which are nor None nor in `attrs`
+excluded_not_none_predicate = lambda key, value, excluded_attrs: \
+    excluded_attrs_predicate(key, excluded_attrs) and attr_not_none_predicate(value)
+# Attributes which are not empty
+# TODO
+# Attributes which are not None, not in `attrs` and not empty
+# TODO
 
 class Summary:
     """ Base class or all summaries"""
@@ -25,5 +42,53 @@ def write_json(filename, summary):
         :param summary: summary exported from a SummaryBuilder
         :type summary: dict
     """
+    print(summary)
     with open(filename, 'w') as file:
         file.write(dumps(summary, indent=4))
+
+
+def select_method(summary, predicate, **kwargs):
+    """
+        Creates the summary's summary excluding parameters if necessary
+
+        :param summary: summary from with the summary will be created
+        :type summary: SummaryBuidlder
+        :param predicate: predicate matching the attributes to collect
+        :type predicate: lambda
+        :param kwargs: parameters needed by the predicate
+
+        :raises ?: attributes not in kwargs
+
+        :returns: the summary matching the requirements
+        :rtype: dict
+    """
+    
+    if 'excluded_attrs' not in kwargs:
+        kwargs['excluded_attrs'] = []
+    print(kwargs['excluded_attrs'])
+    # Parameters needed by the predicate
+    needed_params = [param.name for param in inspect.signature(predicate).parameters.values()]
+
+    def method(summary, predicate, params):
+        """ Returns the summary's summary attributes matching the predicate """
+        result = {}
+
+        for (key, value) in vars(summary).items():
+            tmp_params = params[:]
+
+            if 'key' in needed_params:
+                tmp_params.insert(needed_params.index('key'), key)
+            if 'value' in needed_params:
+                tmp_params.insert(needed_params.index('value'), value)
+
+            if predicate(*tmp_params):
+                result[key] = value
+
+        return result
+
+    # TODO case arg not in kwargs
+    # Parameters `key` and `value` will be processed afterwards in the function `method`
+    params = [kwargs[param] for param in needed_params if param not in ('key', 'value')]
+    tmp  = method(summary, predicate, params)
+    print("TMP", tmp)
+    return tmp
---
+++ pyparser/summary/summary_visitor.py
@@ -0,0 +1,92 @@
+from collections.abc import Iterable
+from pyparser.summary.summary import (
+    Summary,
+    all_attrs_predicate as all_pre,
+    excluded_attrs_predicate as pre,
+    select_method as sm
+)
+
+class SummaryVisitor:
+    PARAMETERS_FIELDS = ['select_method', 'excluded_attrs', 'predicate']
+
+    def __init__(self, parameters=None, defaults=None):
+        """ 
+            parameters -> {
+                aSummaryClassName: {
+                    select_method: function,
+                    excluded_attrs: [],
+                    predicate: lambda
+                }
+            }
+        """
+        self.parameters =  {
+            'BodySummary': {
+                'excluded_attrs': ['summary_list']
+            }
+        }
+
+        if parameters is not None:
+            self.parameters.update(parameters)
+
+        self.defaults = defaults or {
+            'select_method': sm,
+            'predicate': pre,
+            'excluded_attrs': []
+        }
+
+    def visit(self, summary):
+        """ Visits a summary """
+        method = f'visit_{summary.__class__.__name__.lower()}'
+        visitor = getattr(self, method, self.generic_visit)
+        return visitor(summary)
+
+    def generic_visit(self, summary):
+        """ Called if no explicit visitor function exists for a node """
+        if isinstance(summary, Summary):
+            raise Warning(f'No method have been define for the class {summary.__class__.__name__}')
+        else:
+            raise TypeError('Summary or child class object expected')
+    
+    def _get_parameters(self, name):
+        return self.parameters[name] if name in self.parameters else None
+    
+    def _auto_build(self, summary):
+        """ Generates a basic summary """
+        field_map = {}
+        summary_name = summary.__class__.__name__
+
+        for field in self.PARAMETERS_FIELDS:
+            try:
+                field_map[field] = self._get_parameters(summary_name)[field]
+            except TypeError: # result can be None
+                field_map[field] = self.defaults[field]
+            except KeyError: # or incomplete
+                field_map[field] = self.defaults[field]
+
+        print(field_map)
+        return summary.build(**field_map)
+    
+    def _safe_build(self, summary, output, attr):
+        if attr in vars(summary):
+            attribute = getattr(summary, attr)
+            output[attr] = [self.visit(field) for field in attribute] \
+                if isinstance(attribute, Iterable) else self.visit(attribute)
+    
+    def visit_bodysummary(self, summary):
+        result = self._auto_build(summary)
+        self._safe_build(summary, result, 'functions')
+        self._safe_build(summary, result, 'fors')
+
+        return result
+    
+    def visit_functionsummary(self, summary):
+        result  = self._auto_build(summary)
+        self._safe_build(summary, result, 'body')
+
+        return result
+    
+    def visit_forsummary(self, summary):
+        result = self._auto_build(summary)
+        self._safe_build(summary, result, 'body')
+
+        return result
